a2e3e4d04d37ed10083a2673737cd3a82c831e3c
Varun Mendre <varunmm0404@gmail.com>
2026-02-17 06:36:47 +0530

    BOM

---
+++ 01_Brower-Object-Model/script.js
@@ -0,0 +1,12 @@
+// // const list = document.querySelector("ul");
+
+// // list.addEventListener("click", (e) => {
+// //     location.reload();
+// // });
+
+// // console.log("hello");
+
+// console.log(`Height: ${innerHeight}`);
+// console.log(`Width : ${innerWidth}`);
+// console.log(`Outer Height: ${outerHeight}`);
+// console.log(`Outer Width : ${outerWidth}`);
