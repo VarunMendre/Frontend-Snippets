c5ed752f5fa975eb4b1b780d89b748183ecf5337
Varun Mendre <varunmm0404@gmail.com>
2026-04-08 20:28:46 +0530

    imple: Components, Props, Default Props, Wrapper Component

---
+++ 21_Home-Page-and-Props/vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss({
+      content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+      theme: {
+        extend: {
+          fontFamily: {
+            sans: ["Roboto", "sans-serif"],
+          },
+          gridTemplateColumns: {
+            "70/30": "70% 28%",
+          },
+        },
+      },
+      plugins: [],
+    }),
+  ],
+
+  server: {
+    port: 3000,
+  },
+});
