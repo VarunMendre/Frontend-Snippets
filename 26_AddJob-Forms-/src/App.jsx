ba05f70e45d61a2fec45381b61d0de0339e72ed0
Varun Mendre <varunmm0404@gmail.com>
2026-04-13 20:46:13 +0530

    feat: adding new jobs & deleting jobs, uses useNavigate() hook to redirect

---
+++ 26_AddJob-Forms-/src/App.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import JobPage, { jobLoader } from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
+
+const App = () => {
+  // add new job
+  const addJob = async (newJob) => {
+    await fetch("/api/jobs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newJob),
+    });
+
+    return;
+  };
+
+  // delete job
+
+  const deleteJob = async (id) => {
+     await fetch(`/api/jobs/${id}`, {
+      method: "DELETE",
+    });
+
+    return;
+  }
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/jobs" element={<JobsPage />} />
+          <Route
+            path="/add-job"
+            element={<AddJobPage addJobsSubmit={addJob} />}
+          />
+          <Route
+            path="/jobs/:id"
+            element={<JobPage deleteJob={deleteJob} />}
+            loader={jobLoader}
+          />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </>,
+    ),
+  );
+  return <RouterProvider router={router} />;
+};
+
+export default App;
+
+/**<>
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListing />
+      <ViewJobs />
+    </> */
+
+// new Job list component, used useState() hook to toggle description, setup of React Router
